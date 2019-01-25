84f3f5b78be9e1246d900bf4aa98ea73263e1f5f
Alexkcshing <alexkcshing@gmail.com>
2019-01-25 17:17:02 +0000

    pass room play next tests

--- room.rb
+++ room.rb
@@ -1,8 +1,15 @@
 class Room
+  attr_reader :guests, :size, :playlist, :current_song
 
-  def initialize()
+  def initialize(guests, size, playlist)
+    @guests = guests
+    @size = size
+    @playlist = playlist
+    @current_song = current_song
   end
 
+  def play_next
+    @current_song = @playlist.play_next
+  end
 
-  
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -3,6 +3,7 @@ require("minitest/rg")
 require_relative("../guest")
 require_relative("../song")
 require_relative("../playlist")
+require_relative("../room")
 
 class TestRoom < MiniTest::Test
 
@@ -22,10 +23,20 @@ class TestRoom < MiniTest::Test
     @playlist.add_song(@song_2)
     @playlist.add_song(@song_3)
 
-
-    @room_1 = Room.new()
+    @guests = [@guest_1, @guest_2, @guest_3]
+    @room_1 = Room.new(@guests, 6, @playlist)
 
   end
 
+  def test_getters
+    assert_equal(@guests, @room_1.guests)
+    assert_equal(6, @room_1.size)
+    assert_equal(@playlist, @room_1.playlist)
+  end
 
+  def test_play_next_song_in_playlist
+    @room_1.play_next
+    assert_equal(@song_1, @room_1.current_song)
+  end
+  
 end
