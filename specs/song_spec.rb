d492fdce29edef1966a2945118e48b7caa39831b
Alexkcshing <alexkcshing@gmail.com>
2019-01-25 13:41:57 +0000

    pass get artist test

--- specs/song_spec.rb
+++ specs/song_spec.rb
@@ -18,5 +18,8 @@ class TestSong < MiniTest::Test
   def test_play_song
     assert_equal("Tongues intro...", @song_1.play) #not just an attr reader, has to actually play the song
   end
-  
+
+  def test_get_song_artist
+    assert_equal("Joywave", @song_1.artist)
+  end
 end
