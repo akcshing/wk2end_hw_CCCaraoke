866d5a6cb80b6e82faa2bfdd77a82096586c6568
Alexkcshing <alexkcshing@gmail.com>
2019-01-25 14:33:46 +0000

    pass get tests

--- guest.rb
+++ guest.rb
@@ -1,12 +1,12 @@
 class Guest
   attr_reader :name, :skills, :fav, :money
 
-  def initialize(name, skills, :fav, :money)
+  def initialize(name, skills, fav, money)
     @name = name
     @skills = skills
     @fav = fav
     @money = money
   end
-  
+
 
 end
--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -1,6 +1,7 @@
 require("minitest/autorun")
 require("minitest/rg")
 require_relative("../guest")
+require_relative("../song")
 
 class TestGuest < MiniTest::Test
 
@@ -12,5 +13,10 @@ class TestGuest < MiniTest::Test
 
   end
 
-
+  def test_get_guest_attr
+    assert_equal("Alex", @guest_1.name)
+    assert_equal(true, @guest_1.skills)
+    assert_equal(@song_1, @guest_1.fav)
+    assert_equal(100, @guest_1.money)
+  end
 end
