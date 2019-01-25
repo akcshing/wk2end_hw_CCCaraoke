ecc37113e49be1bd9277e83b7ed1a8efe1818db9
Alexkcshing <alexkcshing@gmail.com>
2019-01-25 18:02:34 +0000

    change guests and playlist to hard code instead of initialize parameters

--- room.rb
+++ room.rb
@@ -1,17 +1,27 @@
 class Room
-  attr_reader :guests, :size, :playlist, :current_song :song_list
+  attr_reader :guests, :size, :playlist, :current_song, :song_list
 
-  def initialize(guests, size, playlist, song_list)
-    @guests = guests
+  def initialize(size, song_list)
+    @guests = []
     @size = size
-    @playlist = playlist
+    @playlist = []
     @current_song = current_song
     @song_list = song_list
   end
+
+  def add_song(song_title, song_artist)
+    @playlist << @song_list.find{|song| song.title == song_title}
+
+  end
+
+
 
   def play_next
     @current_song = @playlist.play_next
   end
 
+  def check_in
+    @guests
+  end
 
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -18,36 +18,56 @@ class TestRoom < MiniTest::Test
     @guest_2 = Guest.new("Stacey", true, @song_2, 20)
     @guest_3 = Guest.new("Michael", false, @song_3, 40)
 
-    @playlist = Playlist.new
-    @playlist.add_song(@song_1)
-    @playlist.add_song(@song_2)
-    @playlist.add_song(@song_3)
+    # @playlist = Playlist.new
+    # @playlist.add_song(@song_1)
+    # @playlist.add_song(@song_2)
+    # @playlist.add_song(@song_3)
+
+    @song_list = [@song_1, @song_2, @song_3, @song_4]
 
     @guests = [@guest_1, @guest_2, @guest_3]
-    @room_1 = Room.new(@guests, 6, @playlist)
-    @room_2 = Room.new([], 4, @playlist)
+    # @room_1 = Room.new(@guests, 6, @playlist, @song_list)  # removing guests parameter
+
+    @room_2 = Room.new(4, @song_list)
+
   end
 
   def test_getters
-    assert_equal(@guests, @room_1.guests)
-    assert_equal(6, @room_1.size)
-    assert_equal(@playlist, @room_1.playlist)
+    # assert_equal(@guests, @room_1.guests)
+    assert_equal(4, @room_2.size)
+    assert_equal(@song_list, @room_2.song_list)
   end
 
-  def test_play_next_song_in_playlist
-    @room_1.play_next
-    assert_equal(@song_1, @room_1.current_song)
+  def test_add_song_from_song_list_to_playlist
+    @room_2.add_song("Always", "blink-182")
+    assert_equal(@song_4, @room_2.playlist[0])
   end
 
-  def test_check_in_guests
+  # def test_play_next_song_in_playlist
+  #   @room_2.add_song(@song_1)
+  #   @room_2.play_next
+  #   assert_equal(@song_1, @room_2.current_song)
+  # end
 
+  def test_count_guests__empty
+    assert_equal([], @room_2.guests)
   end
 
-  def test_check_out_guests
+  # def test_check_in_guests
+  #   @room_2.check_in(@guest_1)
+  #   @room_2.check_in(@guest_3)
+  #   assert_equal(2, @room_2.guests.count)
+  # end
 
-  end
-
-  def test_add_song_to_song_list
-
-  end
+  # def test_check_out_guests
+  #
+  # end
+  #
+  # def test_add_song_to_song_list
+  #
+  # end
+  #
+  # def test_find_song_in_song_list
+  #
+  # end
 end
